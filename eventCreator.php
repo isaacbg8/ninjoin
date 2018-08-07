909e46866db59369a95a1574b60fb362696f24f8
klmader <maderk@bc.edu>
2018-08-07 16:37:56 -0700

    I commit

--- eventCreator.php
+++ eventCreator.php
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "INSERT INTO events (id,eventName,location,currentNumNinjas,minNinjasForSuccess,maxNinjas,details) VALUES (5,'test','sere',3,4,5,'sd')";
+$sql = "INSERT INTO events (id,eventName,location,currentNumNinjas,minNinjasForSuccess,maxNinjas,details) VALUES (6,'test','sere',3,4,5,'sd')";
 
 
 if($conn->query($sql) == TRUE) {
